b0ef014e173123f34b6b5b1e097f69b410297ec2
TylerBac0ng <tbacong@cpp.edu>
2025-04-22 23:36:22 -0700

    Update Data_Cleansing.py
    added false positive filter, removes duplicates based off id, severity identification for crimes, and gets date of crimes

--- Data_Cleansing.py
+++ Data_Cleansing.py
@@ -15,6 +15,28 @@ df['Description'] = df['Description'].astype(str).str.lower()
 # Filter rows where any keyword appears in the Description
 gun_crimes = df[df['Description'].str.contains('|'.join(gun_keywords), na=False)]
 
+# Filter rows where to exclude non-gun weapons
+gun_crimes = gun_crimes[~gun_crimes['Description'].str.contains('knife|blunt|taser|weapon - other', na=False)]
+
+# Removes duplicate rows based on 'ID' column (Assumes 'ID' is a unique identifier for each crime incident)
+gun_crimes = gun_crimes.drop_duplicates(subset=['ID'])
+
 # Optionally reset index and preview
 gun_crimes = gun_crimes.reset_index(drop=True)
 print(gun_crimes.head())
+
+# Classify severity of crime
+def classify_severity(crime_type):
+	if any(x in crime_type.lower() for x in ['homicide', 'murder']):
+		return 'Severe'
+	elif any(x in crime_type.lower() for x in ['robbery', 'assault']):
+		return 'High'
+	else:
+		return 'Medium'
+        
+gun_crimes['Severity'] = gun_crimes['Primary Type'].apply(classify_severity)
+
+# Get day, month, year of crimes
+gun_crimes['Year'] = gun_crimes['Date'].dt.year
+gun_crimes['Month'] = gun_crimes['Date'].dt.month
+gun_crimes['Day'] = gun_crimes['Date'].dt.day
